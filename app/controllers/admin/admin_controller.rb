9e9578fd1fa89cf5288fa173f02693d13907f1d6
DaniraKarovic <karovic.danira@gmail.com>
2017-08-12 13:19:09 +0200

    Dashboard for admins

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -8,6 +8,10 @@ class Admin::AdminController < ApplicationController
   end
 
   def dashboard
-
+    @registered_users_count = User.where(role:0).count
+    @artists_count = Artist.all.count
+    @songs_count = Song.all.count
+    @genres_count = Genre.all.count
+    @albums_count = Album.all.count
   end
 end
