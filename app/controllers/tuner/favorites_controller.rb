90c9a4eb8afa28ecb7134dffe079acc6d73dff1d
DaniraKarovic <karovic.danira@gmail.com>
2017-08-17 17:43:04 +0200

    Favorite added to songs with heart icons

--- app/controllers/tuner/favorites_controller.rb
+++ app/controllers/tuner/favorites_controller.rb
@@ -5,10 +5,12 @@ class Tuner::FavoritesController < Tuner::TunerController
 
   def create
     @favorite = Favorite.create(user_id: current_user.id, song_id: params[:song_id])
+    redirect_to tuner_songs_path, notice: 'This song is in your favorites now!'
   end
 
   def destroy
-    @favorite = Favorite.find(parmas[:id]).destroy
+    @favorite = Favorite.where(user_id: current_user.id, song_id: params[:id]).first.destroy
+    redirect_to tuner_songs_path, notice: 'Song is no longer in your favorites!'
   end
 
 end
